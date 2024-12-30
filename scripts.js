31e1437ca038018c9d21c009399aa59fd4750492
Rehjii-Martin <rehjiimartin15@gmail.com>
2024-12-30 12:20:26 -0800

    Added debugging protocols for scripts.js in order to assure random number generation in between range and also reassigned (1,2,3)  to (rock, paper, scissors) more logically

--- scripts.js
+++ scripts.js
@@ -3,14 +3,17 @@ function getComputerChoice () {
     const maxRange = 3; // max value option
 
      // Assign random values to separate variables
-    const choice= Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
-            
+    const choice = Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
+     // Debugging log for random number check
+    console.log(choice);
+
+
     let result;
-        if (choice === 3) {
+        if (choice === 1) {
         result = 'rock'
         } else if (choice === 2) {
         result = 'paper'
-        } else if (choice === 1) {
+        } else if (choice === 3) {
         result = 'scissors'
         }
 
